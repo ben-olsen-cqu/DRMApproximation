8034097f5cb0aebc17562101f32413fe2afcad06
ben-olsen-cqu <ben.olsen@cqumail.com>
2021-06-29 07:51:17 +1000

    Configured Premake

--- premake5.lua
+++ premake5.lua
@@ -1,12 +1,33 @@
 workspace "DRMApproximation"
     configurations {"Debug", "Release"}
+    platforms {"x64"}
+
+if os.host() == "Windows" then
+    filter {"platforms:x64"}
+        system "Windows"
+        architecture "x86_64"
+        defines "WINDOWS"
+end
+
+if os.host() == "macosx" then
+    filter {"platforms:x64"}
+        system "macosx"
+        defines "MAC"
+end
+
+if os.host() == "linux" then
+    filter {"platforms:x64"}
+        system "Linux"
+        architecture "x86_64"
+        defines "LINUX"
+end
 
 project "DRMApproximation"
     kind "ConsoleApp"
     language"C++"
     targetdir "Build/%{cfg.buildcfg}"
 
-   files { "**.h", "**.cpp" }
+   files { "Headers/*.h", "Source/*.cpp" }
 
    filter "configurations:Debug"
       defines { "DEBUG" }
